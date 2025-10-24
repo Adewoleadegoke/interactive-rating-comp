a0c5d30adc27765850a43aa6592dc40af0e116ee
Adewole Adegoke <adewoleadegoke046@gmail.com>
2025-10-24 16:36:03 -0700

    Add files via upload

---
+++ int-rating.js
@@ -0,0 +1,23 @@
+var ratingChosen = document.querySelectorAll('.button-row button');
+var firstCard = document.getElementById('first-display');
+var secondCard = document.getElementById('second-display');
+var ratingText = document.getElementById('select-rating')
+var submitBtn = document.getElementById('submit')
+var ratingPicked = null;
+
+for (var i=0; i < ratingChosen.length; i++) {
+ratingChosen[i].addEventListener('click', function() {
+    ratingPicked = this.textContent;
+})
+}
+
+submitBtn.addEventListener('click', function () {
+    if (ratingPicked) {
+    ratingText.textContent = "You selected " + ratingPicked + " out of 5";
+    firstCard.classList.add('hidden');
+    secondCard.classList.remove('hidden');
+    } else {
+        alert("Pick a rating first");
+    }
+});
+
